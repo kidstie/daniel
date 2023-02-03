f8497488c897977666039bda959ae18bb2490ed5
kidstie <116622318+kidstie@users.noreply.github.com>
2023-02-03 18:29:50 +0300

    Add files via upload

---
+++ pers.js
@@ -0,0 +1,26 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+console.log(header)
+window.onscroll = function() {
+    const top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+// collapse navbar after click on small devices
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+})
+Footer
+© 2023 zelalem,tatus
+Footer navigation
+Terms
+Privacy
+Security
+Status
